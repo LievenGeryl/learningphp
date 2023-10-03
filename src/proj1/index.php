976f8e58d937d53f9af4a26d847f70c3c8ca863c
Lieven Geryl <lieven.geryl@student.howest.be>
2023-10-03 16:04:28 +0200

    Experimentation with objects

--- src/proj1/index.php
+++ src/proj1/index.php
@@ -7,18 +7,50 @@
 </head>
 <body>
     <?php
+        declare(strict_types=1);
+
         $some_string = 'hello world';
 
         $some_bool = False;
 
-    if($some_bool)
-    {
-        echo 'Wow, some_bool was true!';
-    }
-    else
-    {
-        echo $some_var;
-    }
+
+        if($some_bool)
+        {
+            echo 'Wow, some_bool was true!';
+        }
+        else
+        {
+            echo $some_var;
+        }
+
+        class Person
+        {
+            public string $name;
+            public int $age;
+
+            function __construct($name , $age) 
+            {
+                $this->name = $name;
+                $this->age = $age;
+            }
+
+            public function GetName()
+            {
+                return $this->name;
+            }
+
+            public function DoesSomething(){
+                echo $this->name . " is doing something.";
+            }
+
+        }
+        $bob = new Person("bob", 43);
+        echo $bob->name;
+        echo $bob->age;
+        echo $bob->DoesSomething();
+        $dsn = "hello world"; //connection string
+        $test = new PDO($dsn);
+
     ?>
 </body>
 </html>
