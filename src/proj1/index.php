8f4dabad6a05668b7b207a9d03cdda0f1d420877
Lieven Geryl <lieven.geryl@student.howest.be>
2023-10-03 09:36:21 +0200

    adds hello world

---
+++ src/proj1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning PHP - Lieven</title>
+</head>
+<body>
+    <?php
+        $someVar = 'hello world';
+        echo $someVar;
+    ?>
+</body>
+</html>
